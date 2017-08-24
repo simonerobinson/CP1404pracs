26a816f1a67acf81e18d3d166d7b70ddf68d74fd
simonerobinson <simone.robinson@my.jcu.edu.au>
2017-08-24 10:26:37 +1000

    Prac code

--- Week 5/total_income.py
+++ Week 5/total_income.py
@@ -1,1 +1,36 @@
-print("code is hard")
+# # #print("code is hard")
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# #
+# # numbers[0] = "ten"
+# # print(numbers)
+# # #numbers[6] = 1
+# # #print(numbers)
+# sim2 = numbers[2:]
+# print(sim2)
+# sim = 9 in numbers
+# print(sim)
+#
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
